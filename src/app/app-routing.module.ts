9da3149322338fdd4d22cc7e2043707cea4e7071
Tiagosantos056 <tiagosantos0546@gmail.com>
2021-10-30 19:35:07 -0300

    Tarefa: Criando o modulo de cursos e usando roteamento com lazy loading

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { CoursesModule } from './courses/courses.module';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+}];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+
+})
+
+export class AppRoutingModule { }
