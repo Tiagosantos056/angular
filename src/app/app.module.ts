bd47fc8011647f4e9908a2aacce97eece322131c
Tiagosantos056 <tiagosantos0546@gmail.com>
2022-02-28 14:28:40 -0300

    Correcao do component routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 
@@ -12,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    RouterModule,
     BrowserAnimationsModule,
     MatToolbarModule
   ],
